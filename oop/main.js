c1b613c0d8b2fe4c2c250bc2519a12f1f653022a
niyobertin <niyonkurubbertin@gmail.com>
2022-02-16 21:33:35 -0800

    prottyoe inhertnance

--- oop/main.js
+++ oop/main.js
@@ -27,12 +27,53 @@ class Car{
 
 const tructor = new Car("fuso",'278 meters','2000kg');
 for(element in tructor){
-    console.log(element +" : "+tructor[element])
+   // console.log(element +" : "+tructor[element])
 }
-tructor.showDetails();
+//tructor.showDetails();
 
 
 //statick method
-//statifck methods is a method used to creat utillity function
+//static methods is a method used to creat utillity function.
+
+//example of static method
+
+class Staticaamethod{
+static onCall(){
+    return "this is a static methods";
+}
+}
+//creating instance for demo
+const stat = new Staticaamethod();
+//calling static methods
+
+//console.log(Staticaamethod.onCall());
+
+//inhertance in javacript;
+
+//inheritnance helps to access properties of old class with the help of child class
+//example
+
+
+class Person{
+    constructor(fname,lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
+//creating child class
+
+class Discription extends Person{
+   constructor(job,salary){
+       super("Niyonkuru","Bertin");
+       this.job = job;
+       this.salary = salary;
+   }
+   show(){
+       console.log(`${this.fname} ${this.lname} I am a ${this.job} and with ${this.salary} rewards per day`)
+   }
+}
+
+const aline = new Discription("student","$0.9");
+aline.show();
 
 
